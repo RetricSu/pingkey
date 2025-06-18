6023069369775bf38ae08eb56df145e9e8796cea
RetricSu <inneverland2013@gmail.com>
2025-06-18 09:54:22 +0800

    feat: add action btns area in profile page

--- app/setting/page.tsx
+++ app/setting/page.tsx
@@ -335,7 +335,7 @@ function SettingPage() {
               onClick={() => setIsEditingRelays(true)}
               className="text-xs text-neutral-600 dark:text-neutral-400 hover:text-neutral-900 dark:hover:text-neutral-100 transition-colors"
             >
-              Edit Relays
+              Edit
             </button>
           )}
         </div>
